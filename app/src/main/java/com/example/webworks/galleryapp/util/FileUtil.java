4165fec7473a4f6f833e101c9e79bb63fc3f5fcd
webworks <jitendra.tiwari@neosofttech.com>
2018-07-25 15:57:21 +0530

    Removed git ignore gf

--- app/src/main/java/com/example/webworks/galleryapp/util/FileUtil.java
+++ app/src/main/java/com/example/webworks/galleryapp/util/FileUtil.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
@@ -30,21 +29,29 @@ public class FileUtil {
 
     public Boolean deletFiles(String path){
         File files=new File(path);
-
-        if (files.exists())
+        /*if (files.exists())
         {
             return files.delete();
-        }
+        }*/
         return false;
     }
-    public void copyFile(File sourceFile, File destFile) throws IOException {
-        OutputStream out;
-        File file = new File(destFile + "Folder Name" + File.separator + sourceFile);
-        file.createNewFile();
-        out = new FileOutputStream(file);
+    private void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!sourceFile.exists()) {
+            return;
+        }
 
-        out.write(0);
-        out.close();
+        FileChannel source = null;
+        source = new FileInputStream(sourceFile).getChannel();
+        FileChannel destination = new FileOutputStream(destFile).getChannel();
+        if (destination != null && source != null) {
+            destination.transferFrom(source, 0, source.size());
+        }
+        if (source != null) {
+            source.close();
+        }
+        if (destination != null) {
+            destination.close();
+        }
     }
 
 }
